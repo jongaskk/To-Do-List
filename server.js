70113a919a93aa596999fd86fa6880857639c3ed
jongaskk <joaogustavogetao@gmail.com>
2025-10-09 12:27:34 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -1,25 +1,59 @@
-import sqlite3 from "sqlite3";
-import { open } from "sqlite";
-import express from "express";
+import sqlite3 from "sqlite3";                                                            
+import { open } from "sqlite";                                                            
+import express from "express";               
 
 const app = express();
 const PORT = 3000;
 
 //middleware
 app.use(express.json());
-app.use(express.static('public/')); // serve apara acessar arquivos estáticos(HTML, CSS e JS)
+app.use(express.static('public/')); // serve para acessar arquivos estáticos(HTML, CSS e JS)
 
 // inicializando o banco de dados SQLite
 let db;
 
-async function initDB(){
+async function initDB() {
     db = await open({
-        filename: '.banco.db',
+        filename:  './banco.db',
         driver: sqlite3.Database,
     });
 
     await db.run(`CREATE TABLE IF NOT EXISTS tasks(
-        id INTEGER PRIMARY KEY AUTOINCREMENT, description
-        TEXT NOT NULL, completed INTEGER DEFAULT 0
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        description TEXT NOT NULL,
+        completed INTEGER DEFAULT 0
     )`);
 }
+
+// API Endpoints
+app.get('/tasks', async(req, res) => {
+    const tasks = await db.all()
+    res.json(tasks)
+});
+
+app.post('/tasks', async(req, res) => {
+    const { description } = req.body;
+    const stmt = await db.prepare(`INSERT INTO tasks (description) VALUES (?)`);
+    await stmt.run(description);
+    await stmt.finalize();
+    res.status(201).json({ message: 'Task added' });
+});
+
+app.delete('/tasks', async(req, res) => {
+    const { id } = req.params;
+    await db.get(`DELETE FROM tasks WHERE id = ?`, id);
+    res.status(204).send();
+})
+
+app.patch('/task/:id/toggle', async(req, res) => {
+    const { id } = req.params;
+    const task = await db.get(`SELECT * FROM tasks WHERE id = ?`, id);
+    const completed = task.completed ? 0 : 1;
+    await db.run(`UPDATE tasks SET completed = ? WHERE id = ?`, completed, id);
+    res.status(204).send();
+});
+
+app.listen(PORT, async() => {
+    await initDB();
+    console.log(`Server running at http://localhost:${PORT}`)
+})
